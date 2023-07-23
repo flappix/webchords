3923d5a9b8bb34320025a5e860f8b36f11828bcf
lappe <lappe@mpib-berlin.mpg.de>
2023-07-23 02:44:24 +0200

    click on chord to start from this one

--- FluidR3_GM/download.sh
+++
@@ -1,4 +0,0 @@
-sf=$1
-svn checkout https://github.com/gleitz/midi-js-soundfonts/trunk/FluidR3_GM/$sf-mp3
-svn export https://github.com/gleitz/midi-js-soundfonts/trunk/FluidR3_GM/$sf-mp3.js
-svn export https://github.com/gleitz/midi-js-soundfonts/trunk/FluidR3_GM/$sf-mp3.js.gz
